389438fbd9f85e7f3ddc3ac2c183156aec009354
DokheTHIOUB <thioub1901@gmail.com>
2023-11-14 17:51:55 +0000

    resolve conflit

--- resources/views/Eleves/modifierEleve.blade.php
+++ resources/views/Eleves/modifierEleve.blade.php
@@ -1,66 +1,38 @@
 @include('nav')
-<<<<<<< HEAD
 <!-- jdkjdkdkdjddk -->
-=======
->>>>>>> 21620d14b188f8e8a4d1636698261be26155fd76
 <div class="container">
     <div class="card">
         <div class="col-md-6 offset-3 mt-5">
             <h5 class="card-header text-center bg-dark text-white">AJOUT ELEVE</h5>
             <div class="card-body">
-<<<<<<< HEAD
                 <form method="post" action="/eleve/modifierEleve/{{$eleve->id}}">
                     @csrf
                     <div class="form-group">
                         <label for="nomEleve">Nom Eleve</label>
-                        <input type="text" class="form-control" id="nomEleve" value="{{$eleve->nomEleve}}"
-=======
-                <form method="post" action="/ajouterEleve">
-                    @csrf
-                    <div class="form-group">
-                        <label for="nomEleve">Nom Eleve</label>
-                        <input type="text" class="form-control" id="nomEleve" placeholder="Enter un nom"
->>>>>>> 21620d14b188f8e8a4d1636698261be26155fd76
-                            name="nomEleve">
+                        <input type="text" class="form-control" id="nomEleve" value="{{$eleve->nomEleve}}">
                     </div>
                     <div class="form-group">
                         <label for="prenomEleve">Prénom Eleve</label>
-<<<<<<< HEAD
-                        <input type="text" class="form-control" id="prenomEleve" value="{{$eleve->prenomEleve}}"
-=======
-                        <input type="text" class="form-control" id="prenomEleve" placeholder="Enter un prénom"
->>>>>>> 21620d14b188f8e8a4d1636698261be26155fd76
-                            name="prenomEleve">
+                        <input type="text" class="form-control" id="prenomEleve" value="{{$eleve->prenomEleve}}">
                     </div>
                     <div class="form-group">
                         <label for="classe">classe</label>
-<<<<<<< HEAD
-                        <input type="text" class="form-control" id="classe" value="{{$eleve->classe}}"
-=======
-                        <input type="text" class="form-control" id="classe" placeholder="Enter votre classe"
->>>>>>> 21620d14b188f8e8a4d1636698261be26155fd76
-                            name="classe">
+                        <input type="text" class="form-control" id="classe" value="{{$eleve->classe}}">
                     </div>
                     <div class="form-group">
                         <label for="dateNaiss">Date De Naissance</label>
-<<<<<<< HEAD
                         <input type="date" class="form-control" id="dateNaiss" value="{{$eleve->date_de_naissance}}" name="dateNaiss"> 
-=======
                         <input type="date" class="form-control" id="dateNaiss" name="dateNaiss">
->>>>>>> 21620d14b188f8e8a4d1636698261be26155fd76
                     </div>
 
                     <div class="form-group">
                         <label for="sexeEleve">Sexe</label>
                         <select name="sexe" id="" class="form-control">
-<<<<<<< HEAD
                             <option value="masculin" >Masculin</option>
                             <option value="féminin" >Féminin</option>
-=======
                             <option value="">Choisissez</option>
                             <option value="masculin">Masculin</option>
                             <option value="féminin">Féminin</option>
->>>>>>> 21620d14b188f8e8a4d1636698261be26155fd76
                         </select>
                     </div>
                     <button type="submit" class="btn btn-dark offset-4 mt-2">Soumettre</button>
