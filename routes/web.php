bff236e97d852f33ffd24dc68be1c7850839b4be
aicha-ndiaye <aicha8420@gmail.com>
2023-11-14 16:44:12 +0100

    modification bouton supprimer

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use App\Http\Controllers\EleveController;
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -33,13 +23,10 @@ Route::post('/ajouterNote',[NoteController::class,'store']);
 
 
 Route::get('/modifierNote/{id}', [NoteController::class,'edit']);
-<<<<<<< HEAD
 Route::get('/modifie/eleve/{id}', [EleveController::class,'modifieEleve']);
 Route::get('/suppEleve/{id}', [EleveController::class,'destroy']);
 route::post('/modifNote/{id}',[NoteController::class,'update']);
 route::get('/supprimerNote/{id}',[NoteController::class,'delete']);
-=======
 Route::post('/modifNote/{id}', [NoteController::class,'update']);
 Route::get('/modifie/eleve/{id}', [EleveController::class,'modifieEleve']);
 Route::get('/suppEleve/{id}', [EleveController::class,'destroy']);
->>>>>>> 53235fe98b1764a994fce818c99485aaf5034fec
